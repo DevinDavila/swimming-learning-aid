b45e1664bd7cddac3e0c100966752a7c1f9d7cca
Devin Davila <devin.ricardo05@gmail.com>
2023-02-05 20:53:19 +0000

    recreated learner page using react

---
+++ src/frontend/src/Pages/Learner/Learner.jsx
@@ -0,0 +1,33 @@
+import './Learner.css';
+
+function Learner() {
+    return (
+        <div className='learner-container'>
+            <div class="learner-login-section">
+                <div class="learner-login-section-title">Learner login</div>
+                <form class="learner-login-form">
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email address"/>
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <div class="learner-login-submit-button-container">
+                        <button type="submit" class="btn btn-primary learner-login-submit-button">Login</button>
+                    </div>
+                    <div class="learner-login-submit-text">
+                        Don't have an account?
+                        <a href="file:///C:/Users/devin/OneDrive/Desktop/Documents/School/Computer%20Science/Visual%20Studio/frontend/html/learner-sign-up.html" class="learner-login-submit-text-link">Sign up here</a>
+                    </div>
+                </form>
+            </div>
+
+            <a class="learner-back-button" href="file:///C:/Users/devin/OneDrive/Desktop/Documents/School/Computer%20Science/Visual%20Studio/frontend/html/login-decision.html">
+                Back to decision page</a>
+        </div>
+    );
+}
+
+export default Learner;
