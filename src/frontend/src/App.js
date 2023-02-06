7f7922e904d0a324dd69c47d25cf778845af3976
John Z <johnziss9@gmail.com>
2023-02-06 12:34:42 +0000

    I renamed the Home page to Stages page and the new Home page is now the old login decision page.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,11 @@
 import Home from './Pages/Home/Home';
 import Learner from './Pages/Learner/Learner';
+import Stages from './Pages/Stages/Stages';
 
 function App() {
   return (
     <div>
-      <Learner />
+      <Home />
     </div>
   );
 }
