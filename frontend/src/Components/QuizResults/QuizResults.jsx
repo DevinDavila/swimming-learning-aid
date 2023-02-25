9bc1b89575db6ec817f534eb0db251aa5fd0d448
John Z <johnziss9@gmail.com>
2023-02-25 18:42:53 +0000

    Called QuizResults after the Quiz and passed the number of correct answers and the stage.

--- frontend/src/Components/QuizResults/QuizResults.jsx
+++ frontend/src/Components/QuizResults/QuizResults.jsx
@@ -1,32 +1,30 @@
 import './QuizResults.css';
 
-function QuizResults() {
+function QuizResults(props) {
 
-    const QuizScore = 12;
-    const Percentage = Math.floor((QuizScore / 15) * 100);
+    let correctAnswers = props.correctAnswers;
 
+    const Percentage = Math.floor((correctAnswers / 15) * 100);
+    
     return (
         <div className='quiz-result-container'>
             <div className='quiz-result-sections-container'>
                 <div className='quiz-result-title-box'>
-                    <div className='quiz-stage-text'>Stage X Quiz</div>
+                    <div className='quiz-stage-text'>Stage {props.stage} Quiz</div>
                     <div className='line'></div>
-                    <div className='quiz-result-text'>Your score: {QuizScore}/15 ({Percentage}%)</div>
+                    <div className='quiz-result-text'>Your score: {correctAnswers}/15 ({Percentage}%)</div>
                     <div className='quiz-result-text'>Pass mark: 10/15 (66%)</div>
                 </div>
 
                 <div className='response-box'>
-                    {QuizScore > 9 ?
+                    {correctAnswers > 9 ?
                         <div className='quiz-response-text'>
                             Congratulations, you have passed this quiz and are ready to move on.
                         </div>
                         :
-                        <>
-                            {/* If score is 9 or less */}
-                            <div className='quiz-response-text'>
-                                Unfortunately, you have failed this test, give it another go!
-                            </div>
-                        </>
+                        <div className='quiz-response-text'>
+                            Unfortunately, you have failed this test, give it another go!
+                        </div>
                     }
                 </div>
 
