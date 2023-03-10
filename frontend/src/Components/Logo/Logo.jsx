6d25faa51c0de060fa3bf78332db374b0de3b7b6
Devin Davila <devin.ricardo05@gmail.com>
2023-03-10 18:50:52 +0000

    linked the logo to home page

--- frontend/src/Components/Logo/Logo.jsx
+++ frontend/src/Components/Logo/Logo.jsx
@@ -1,16 +1,18 @@
 import './Logo.css';
 import logoImg from '../../images/SE-Logo-RGB.png'
+import { Link } from 'react-router-dom';
+
 
 function Logo() {
     return (
-        <div className='logo'>
+        <Link className="logo" to="/">
             <img src={logoImg} alt="SE-logo-icon" className="logo-picture" />
             <div className="logo-text">
                 <div className="logo-blue-text">Swimming</div>
                 <div className="logo-red-text">Learning</div>
                 <div className="logo-red-text">Aid</div>
             </div>
-        </div>
+        </Link>
     );
 }
 
